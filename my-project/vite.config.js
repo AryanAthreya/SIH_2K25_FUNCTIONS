bbfc3f5809e9c248c489272bec22f0b5eb6fd0d4
Aryan <140130909+AryanAthreya@users.noreply.github.com>
2025-09-17 07:41:12 +0530

    Update vite.config.js

--- my-project/vite.config.js
+++ my-project/vite.config.js
@@ -6,3 +6,8 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [react(),tailwindcss()],
 })
+// vite.config.js
+export default {
+  base: '/SIH_2K25_FUNCTIONS/', // Use your repo name here
+}
+
